552b0a1cd3efb023077eb9ef5f9d489732137358
RomanWuattier <roman.wuattier@gmail.com>
2023-12-28 19:18:04 +0100

    Optimize Day5

--- src/main/java/com/romanwuattier/adventofcode2023/Day5.java
+++ src/main/java/com/romanwuattier/adventofcode2023/Day5.java
@@ -1,6 +1,10 @@
 package com.romanwuattier.adventofcode2023;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
 import java.util.TreeMap;
 import java.util.stream.IntStream;
 
@@ -34,19 +38,16 @@ public class Day5 implements Day {
       maps[i] = map;
     }
 
-    long p1 = Long.MAX_VALUE;
-    for (long seed : seeds) {
-      p1 = Math.min(p1, process(seed, maps));
-    }
+    long p1 = Arrays.stream(seeds).map(seed -> process(seed, maps)).min().orElseThrow();
 
-    // p2 takes time to complete. It can probably be optimized using Ranges instead of generating the seeds.
-    // I will eventually do it when I have time
-    long p2 = Long.MAX_VALUE;
-    for (int i = 0; i < seeds.length; i += 2) {
-      for (long j = seeds[i]; j < seeds[i] + seeds[i + 1]; j++) {
-        p2 = Math.min(p2, process(j, maps));
-      }
+    var seedsRange = IntStream.range(0, seeds.length)
+        .filter(i -> i % 2 == 0)
+        .mapToObj(i -> new Range(seeds[i], seeds[i] + seeds[i + 1] - 1, 0))
+        .toList();
+    for (var map : maps) {
+      seedsRange = process(seedsRange, map);
     }
+    long p2 = seedsRange.stream().mapToLong(r -> r.src).min().orElseThrow();
     return new Output<>(p1, p2);
   }
 
@@ -58,6 +59,35 @@ public class Day5 implements Day {
       }
     }
     return val;
+  }
+
+  private List<Range> process(List<Range> seeds, TreeMap<Long, Range> map) {
+    var q = new LinkedList<>(seeds);
+    var out = new ArrayList<Range>();
+
+    while (!q.isEmpty()) {
+      var seed = q.remove();
+      long seedStart = seed.src;
+      long seedEnd = seed.dst;
+      Optional.ofNullable(map.floorEntry(seedStart)).ifPresentOrElse(floor -> {
+        long srcOverlap = Math.max(seedStart, floor.getValue().src);
+        long dstOverlap = Math.min(seedEnd, floor.getValue().src + floor.getValue().offset - 1);
+        if (dstOverlap > srcOverlap) {
+          if (seedStart < srcOverlap) {
+            q.offer(new Range(seedStart, srcOverlap - 1, 0));
+          }
+          if (seedEnd > dstOverlap) {
+            q.offer(new Range(dstOverlap + 1, seedEnd, 0));
+          }
+          long offset = srcOverlap - floor.getValue().src;
+          out.add(new Range(floor.getValue().dst + offset,
+              floor.getValue().dst + offset + (dstOverlap - srcOverlap + 1), 0));
+        } else {
+          out.add(new Range(seedStart, seedEnd, 0));
+        }
+      }, () -> out.add(new Range(seedStart, seedEnd, 0)));
+    }
+    return out;
   }
 
   record Range(long src, long dst, long offset) {
